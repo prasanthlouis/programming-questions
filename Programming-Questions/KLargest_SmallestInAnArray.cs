80ca61bc7c0b62ed44e3a2d11b5bb980bd2e78e3
unknown <prasanthlouis21@gmail.com>
2017-09-25 19:25:10 -0400

    Solved problem 1

---
+++ Programming-Questions/KLargest_SmallestInAnArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming_Questions
+{
+    public class KLargest_SmallestInAnArray
+    {
+        private int[] _numbers;
+        private int _limit;
+    public KLargest_SmallestInAnArray(int[] numbers, int limit)
+    {
+            _numbers = numbers;
+            _limit = limit;
+    }
+
+        internal void Solution()
+        {
+            Console.WriteLine("Solution 1:");
+             Array.Sort(_numbers);
+            for(int i = 0; i < _limit; i++)
+            {
+                Console.Write(_numbers[i]);
+            };
+        }
+    }
+}
---
+++ Programming-Questions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Programming_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find the k largest or smallest in an array
+            var kLargest_SmallestInAnArray = new KLargest_SmallestInAnArray(new int[] { 3, 6, 7, 8, 4, 5, 2, 1, 0, 9 }, 5);
+            kLargest_SmallestInAnArray.Solution();
+
+            Console.ReadLine();
+        }
+    }
+}
