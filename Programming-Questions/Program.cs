3a689c3d9aabe88a37138b2987d0232323422fcc
unknown <prasanthlouis21@gmail.com>
2017-09-27 21:07:04 -0400

    Reverse a link list in groups of k

--- Programming-Questions/Program.cs
+++ Programming-Questions/Program.cs
@@ -7,45 +7,43 @@ namespace Programming_Questions
     {
         static void Main(string[] args)
         {
-            //Problem 1
-            //Find the k largest or smallest in an array
-            var kLargest_SmallestInAnArray = new KLargest_SmallestInAnArray(new int[] { 3, 6, 7, 8, 4, 5, 2, 1, 0, 9 }, 5);
-            kLargest_SmallestInAnArray.Solution();
-            Console.WriteLine();
+            Problem1();
+            Problem2();
+            Problem3();
+            Problem4();
+            Problem5();
+            Problem6();
+            Problem7();
 
-            //Problem 2
-            //Find a triplet. If found, print true else print false
-            var triplet = new CheckForTriplet(new int[] { 5, 8, 9, 2, 4, 3 });
-            triplet.Solution();
-            Console.WriteLine();
+            Console.ReadLine();
+        }
 
-            var noTriplet = new CheckForTriplet(new int[] { 5, 8, 9, 2, 4, 7 });
-            noTriplet.Solution();
-            Console.WriteLine();
+        private static void Problem8()
+        {
+            throw new NotImplementedException();
+        }
 
-            //Problem 3
-            //Convert a binary tree into a doubly link list
-            var binaryTree = new BinaryTree(10);
-            binaryTree.left = new BinaryTree(12);
-            binaryTree.left.left = new BinaryTree(25);
-            binaryTree.left.right = new BinaryTree(30);
-            binaryTree.right = new BinaryTree(15);
-            binaryTree.right.left = new BinaryTree(36);
-            var convertBinaryTreeToDll = new ConvertBinaryTreeToDll();
-            convertBinaryTreeToDll.MakeADLL(binaryTree);
+        private static void Problem7()
+        {
+            //Problem 7
+            //Implement reversing a link list in groups of (k)
+           var reverseLinkList = new LinkList();
+            reverseLinkList.ReverseLinkListInGroupsOfKS();
 
-            //Problem 4
-            //Find LCA of a BST
-            var lcaBinarySTree = new BinaryTree(20);
-            lcaBinarySTree.left = new BinaryTree(8);
-            lcaBinarySTree.left.left = new BinaryTree(4);
-            lcaBinarySTree.left.right = new BinaryTree(12);
-            lcaBinarySTree.left.right.left = new BinaryTree(10);
-            lcaBinarySTree.left.right.right = new BinaryTree(14);
-            lcaBinarySTree.right = new BinaryTree(22);
-            var lcabst = new LCABST();
-            lcabst.FindLCA(lcaBinarySTree, 10, 14);
+        }
 
+        private static void Problem6()
+        {
+            //Problem 6
+            //Implement a special stack with GetMin() with operation of O(1)
+            var stack = new Stack<int>();
+            var secondStack = new Stack<int>();
+            var getMinStack = new GetMinStack();
+            getMinStack.PerformOperations(stack, secondStack);
+        }
+
+        private static void Problem5()
+        {
             //Problem 5
             //Find LCA of a BT
             var lcaBinaryTree = new BinaryTree(1);
@@ -57,14 +55,53 @@ namespace Programming_Questions
             lcaBinaryTree.right.right = new BinaryTree(7);
             var lcabt = new LCABT();
             lcabt.FindLCA(lcaBinaryTree, 4, 7);
+        }
 
-            //Problem 6
-            //Implement a special stack with GetMin() with operation of O(1)
-            var stack = new Stack<int>();
-            var secondStack = new Stack<int>();
-            var getMinStack = new GetMinStack();
-            getMinStack.PerformOperations(stack, secondStack);
-            Console.ReadLine();
+        private static void Problem4()
+        {
+            //Problem 4
+            //Find LCA of a BST
+            var lcaBinarySTree = new BinaryTree(20);
+            lcaBinarySTree.left = new BinaryTree(8);
+            lcaBinarySTree.left.left = new BinaryTree(4);
+            lcaBinarySTree.left.right = new BinaryTree(12);
+            lcaBinarySTree.left.right.left = new BinaryTree(10);
+            lcaBinarySTree.left.right.right = new BinaryTree(14);
+            lcaBinarySTree.right = new BinaryTree(22);
+            var lcabst = new LCABST();
+            lcabst.FindLCA(lcaBinarySTree, 10, 14);
+        }
+
+        private static void Problem3()
+        {
+            //Problem 3
+            //Convert a binary tree into a doubly link list
+            var binaryTree = new BinaryTree(10);
+            binaryTree.left = new BinaryTree(12);
+            binaryTree.left.left = new BinaryTree(25);
+            binaryTree.left.right = new BinaryTree(30);
+            binaryTree.right = new BinaryTree(15);
+            binaryTree.right.left = new BinaryTree(36);
+            var convertBinaryTreeToDll = new ConvertBinaryTreeToDll();
+            convertBinaryTreeToDll.MakeADLL(binaryTree);
+        }
+
+        private static void Problem2()
+        {
+            //Problem 2
+            //Find a triplet. If found, print true else print false
+            var triplet = new CheckForTriplet(new int[] { 5, 8, 9, 2, 4, 3 });
+            triplet.Solution();
+            Console.WriteLine();
+        }
+
+        private static void Problem1()
+        {
+            //Problem 1
+            //Find the k largest or smallest in an array
+            var kLargest_SmallestInAnArray = new KLargest_SmallestInAnArray(new int[] { 3, 6, 7, 8, 4, 5, 2, 1, 0, 9 }, 5);
+            kLargest_SmallestInAnArray.Solution();
+            Console.WriteLine();
         }
     }
 }
---
+++ Programming-Questions/ReverseLinkListInGroupsOfKcs.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming_Questions
+{
+    public class LinkList
+    {
+        Node head;
+        internal Node ReverseLinkListInGroupsOfK(Node head)
+        {
+            //Limit is 3
+            var current = head;
+            Node next = null;
+            Node previous = null;
+            var count = 0;
+            while (count < 3 && current != null)
+            {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+                count++;
+            }
+            if (next != null)
+                head.next = ReverseLinkListInGroupsOfK(next);
+
+            return previous;
+
+        }
+
+        internal void ReverseLinkListInGroupsOfKS()
+        {
+            CreateLinkList();
+            var heads = ReverseLinkListInGroupsOfK(head);
+            Console.WriteLine("\nSolution 7: Reverse a link list in groups of k");
+            while (heads.next != null)
+            {
+                Console.Write($"{heads._data}->");
+                heads = heads.next;
+            }
+            Console.Write($"{heads._data}");
+            Console.WriteLine();
+        }
+
+        public void push(int v)
+        {
+            var node = new Node(v);
+            node.next = head;
+            head = node;
+        }
+
+        private void CreateLinkList()
+        {
+            push(8);
+            push(7);
+            push(6);
+            push(5);
+            push(4);
+            push(3);
+            push(2);
+            push(1);
+        }
+        public class Node {
+
+            public Node next;
+            public int _data;
+            public Node(int data)
+            {
+                _data = data;
+                next = null;
+            }
+        }
+        
+    }
+}
