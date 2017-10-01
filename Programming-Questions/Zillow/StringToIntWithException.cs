0fc540ac443a9f2860c681297ab43a9595db4823
unknown <prasanthlouis21@gmail.com>
2017-10-01 10:49:39 -0400

    Convert string to int with exception handling

---
+++ Programming-Questions/Zillow/StringToIntWithException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming_Questions.Zillow
+{
+    public class StringToIntWithException
+    {
+        internal void Convert()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Problem 8: Convert string to int with exception handling");
+            string S = "12345";
+            try
+            {
+                Console.Write(int.Parse(S));
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Bad String");
+            }
+        }
+    }
+}
