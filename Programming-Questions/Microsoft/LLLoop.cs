f46b753149fa89835a982797fa286b58ae39c80d
Prasanth Louis <prasanthlouis21@gmail.com>
2018-11-26 04:06:52 -0500

    Update LLLoop.cs

--- Programming-Questions/Microsoft/LLLoop.cs
+++ Programming-Questions/Microsoft/LLLoop.cs
@@ -17,3 +17,22 @@ public class Solution {
         return false;
     }
 }
+
+//Constant space solution
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        if(head == null)
+            return false;
+        var slow = head;
+        var fast = head.next;
+        while(fast != slow)
+        {
+            if(fast == null || fast.next == null)
+                return false;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
+
