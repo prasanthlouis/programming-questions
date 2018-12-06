f9b6ea2917ce6be9b236a303ad8530ff857bc5e0
Prasanth Louis <prasanthlouis21@gmail.com>
2018-12-06 09:01:44 -0500

    Update Remove Duplicates From Array.cs

--- Programming-Questions/Microsoft/Remove Duplicates From Array.cs
+++ Programming-Questions/Microsoft/Remove Duplicates From Array.cs
@@ -31,3 +31,20 @@ public class Solution {
         return count;
     }
 }
+
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if(nums.Count() == 0)
+            return 0;
+        int i =0;
+        for(int j = 1; j< nums.Count(); j++)
+        {
+            if(nums[i] != nums[j])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
