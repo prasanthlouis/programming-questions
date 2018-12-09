223b28b3ae269a786a0df9177701f4ef1ea2b057
Prasanth Louis <prasanthlouis21@gmail.com>
2018-12-09 09:37:14 -0500

    Create Populating Next Right Pointers in Each Node.cpp

---
+++ Programming-Questions/Microsoft/Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(root == NULL)
+            return;
+    TreeLinkNode *pre = root;
+    TreeLinkNode *cur = NULL;
+        while(pre->left)
+        {
+            cur = pre;
+            while(cur)
+            {
+                cur->left->next = cur->right;
+                if(cur->next)
+                {
+                    cur->right->next = cur->next->left;
+                }
+                cur = cur->next;
+            }
+            pre = pre->left;
+        }
+}
+};
