e87b8e0fbf16fc9ec610024fab43d0df36f2d258
Prasanth Louis <prasanthlouis21@gmail.com>
2018-12-17 08:57:43 -0500

    Create LRU Cache.cs

---
+++ Programming-Questions/Microsoft/LRU Cache.cs
@@ -0,0 +1,80 @@
+
+//Time limit exceeded
+public class LRUCache {
+    Dictionary<int,CacheObject> dictionary;
+    int capacity;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dictionary = new Dictionary<int, CacheObject>(capacity);
+    }
+    
+    public int Get(int key) {
+        if(!dictionary.ContainsKey(key))
+            return -1;
+        else{
+            var item = dictionary[key];
+            item.Count = 0;
+            dictionary[key] = item;
+            
+        }
+        foreach(var x in dictionary)
+        {
+            x.Value.Count++;
+        }
+        return dictionary[key].Val;
+    }
+    
+    public void Put(int key, int value) {
+        var cacheObject = new CacheObject(){
+            Val = value,
+            Count = 0
+        };
+        if(dictionary.ContainsKey(key)) {
+            dictionary[key] = cacheObject;
+            Iterate();
+            return;
+            }
+        if(dictionary.Count() == capacity)
+        {
+            var max = int.MinValue;
+            var selectedKey = 0;
+            foreach(var item in dictionary)
+            {
+                if(item.Value.Count > max)
+                {
+                    selectedKey = item.Key;
+                    max = item.Value.Count;
+                }
+            }
+            dictionary.Remove(selectedKey);
+            
+        }
+
+         
+            dictionary.Add(key, cacheObject);
+            
+        Iterate();
+        
+    }
+    
+public void Iterate(){
+    foreach(var x in dictionary)
+        {
+            x.Value.Count++;
+        }
+}
+}
+
+
+
+public class CacheObject{
+    public int Val{get;set;}
+    public int Count{get;set;}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
