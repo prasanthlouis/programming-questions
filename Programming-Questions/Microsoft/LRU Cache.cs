ba99a99a5f614fbe7a8a37c0db702202c8e35061
Prasanth Louis <prasanthlouis21@gmail.com>
2018-12-18 04:01:04 -0500

    Update LRU Cache.cs

--- Programming-Questions/Microsoft/LRU Cache.cs
+++ Programming-Questions/Microsoft/LRU Cache.cs
@@ -1,3 +1,75 @@
+//Faster LRU Cache
+
+public class LRUCache {
+    Dictionary<int,LinkedListNode<CacheObject>> dictionary;
+    LinkedList<CacheObject> linkedList;
+    int capacity;
+    int count = 0;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dictionary = new Dictionary<int, LinkedListNode<CacheObject>>();
+        linkedList = new LinkedList<CacheObject>();
+    }
+    
+    public int Get(int key) {
+        if(!dictionary.ContainsKey(key))
+            return -1;
+        
+        var cacheObject = dictionary[key];
+        if(cacheObject != linkedList.First)
+        {
+            linkedList.Remove(cacheObject);
+            linkedList.AddFirst(cacheObject);
+        }
+        
+        return cacheObject.Value.Val;
+    }
+    
+    public void Put(int key, int value) {
+        var cacheObject = new CacheObject(){
+                Val = value,
+                Key = key
+            };
+        if(!dictionary.ContainsKey(key))
+        {
+            dictionary[key] = linkedList.AddFirst(cacheObject);
+            Console.WriteLine(dictionary[key].Value.Val);
+            if(count == capacity)
+            {
+                var last = linkedList.Last;
+            linkedList.RemoveLast();
+            dictionary.Remove(last.Value.Key);
+            }
+            else{
+                count++;
+            }
+        }
+        else{
+            var item = dictionary[key];
+            item.Value.Val = value;
+            if(item != linkedList.First)
+            {
+                linkedList.Remove(item);
+                linkedList.AddFirst(item);
+            }
+        }
+        
+    }
+}
+
+
+
+public class CacheObject{
+    public int Val{get;set;}
+    public int Key{get;set;}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
 
 //Time limit exceeded
 public class LRUCache {
