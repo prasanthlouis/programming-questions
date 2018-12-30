e65bf8190774b2e3b5fd582c6889a8936fb558a6
Prasanth Louis <prasanthlouis21@gmail.com>
2018-12-30 07:54:48 -0500

    Create   Repeated String Match.cs

---
+++ Programming-Questions/Google/Repeated String Match.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int RepeatedStringMatch(string A, string B) {
+        int count = 1;
+        var ACopy = A;
+        var maxLength = Math.Max(A.Length, B.Length);
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(A);
+        while(stringBuilder.Length < B.Length)
+        {
+            stringBuilder.Append(ACopy);
+            count++;
+        }
+            if(stringBuilder.ToString().Contains(B))
+                return count;
+            stringBuilder.Append(ACopy);
+            if(stringBuilder.ToString().Contains(B))
+                return ++count;
+            return -1;
+    }
+}
