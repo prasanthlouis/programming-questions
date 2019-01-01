19414e09d506a28adab0ee52654f57de069e3e9a
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-01 08:05:44 -0500

    Create RangeAddition.cs

---
+++ Programming-Questions/Google/RangeAddition.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] GetModifiedArray(int length, int[,] updates) {
+        var answer = new int[length];
+        for(int i = 0 ; i < updates.GetLength(0); i++)
+        {
+            var start = updates[i,0];
+            var end = updates[i,1];
+            var addition = updates[i,2];
+            answer[start] += addition;
+            if(end+1 < answer.Length)
+            answer[end+1] -= addition;
+        }
+        int sum = 0;
+        for(int i = 0 ; i < answer.Count() ; i++)
+        {
+            sum += answer[i];
+            answer[i] = sum;
+        }
+        return answer;
+        
+    }
+}
