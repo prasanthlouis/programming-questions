a11255142115ecb38fcee30ef29240a8fc24274e
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-06 05:05:08 -0500

    Update IntersectionOfTwoArrays.cs

--- Programming-Questions/Google/IntersectionOfTwoArrays.cs
+++ Programming-Questions/Google/IntersectionOfTwoArrays.cs
@@ -13,3 +13,40 @@ public class Solution {
         return answerList.ToArray();
     }
 }
+
+
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        var dictionary = new Dictionary<int,int>();
+        var answerDictionary = new Dictionary<int, int>();
+        var answerList = new List<int>();
+        for(int i = 0 ; i < nums1.Count() ; i++)
+        {
+            if(dictionary.ContainsKey(nums1[i]))
+            {
+                dictionary[nums1[i]]++;
+            }
+            else{
+            dictionary.Add(nums1[i],1);
+                }
+        }      
+        for(int i = 0 ; i < nums2.Count(); i++)
+        {
+            if(dictionary.ContainsKey(nums2[i]))
+            {
+                if(answerDictionary.ContainsKey(nums2[i]))
+                {
+                    answerDictionary[nums2[i]]++;
+                }
+                else{
+                    answerDictionary.Add(nums2[i], 1);
+                }
+            }
+        }
+        foreach(var x in answerDictionary)
+        {
+            answerList.Add(x.Key);
+        }
+        return answerList.ToArray();
+    }
+}
