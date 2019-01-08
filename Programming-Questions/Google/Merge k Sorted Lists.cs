302eccc29b8f26bbb4382af505db831d7b9b1a9a
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-08 06:50:39 -0500

    Update Merge k Sorted Lists.cs

--- Programming-Questions/Google/Merge k Sorted Lists.cs
+++ Programming-Questions/Google/Merge k Sorted Lists.cs
@@ -1,3 +1,52 @@
+//Merge Sort
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        var nodes = new List<int>();
+        if(lists.Count() == 0)
+            return null;
+        return Sort(lists, 0, lists.Count() - 1);
+    }
+    
+    public ListNode Sort(ListNode[] lists, int low , int high)
+    {
+        if(low >= high)
+            return lists[low];
+        var mid = (low + high) / 2;
+        var left  = Sort(lists, low, mid);
+        var right = Sort(lists, mid + 1, high);
+        return Merge(left, right);
+    }
+    
+    public ListNode Merge(ListNode l1, ListNode l2)
+    {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        
+        if(l1.val < l2.val)
+        {
+            l1.next = Merge(l1.next, l2);
+            return l1;
+        }
+        
+        else{
+            l2.next = Merge(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
