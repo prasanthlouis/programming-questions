95e5fdb8799c816ac76de1cf74cd9b8b9f50cc99
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-08 05:04:26 -0500

    Update First Missing Positive.cs

--- Programming-Questions/Google/First Missing Positive.cs
+++ Programming-Questions/Google/First Missing Positive.cs
@@ -2,6 +2,38 @@ public class Solution {
     public int FirstMissingPositive(int[] nums) {
         if(nums.Count() == 0)
             return 1;
+        if(nums.Count() == 1)
+            if(nums[0] == 1)
+            {
+                return 2;
+            }else{
+                return 1;
+            }
+        int max = -1;
+        for(int i = 0 ; i < nums.Count() ; i++)
+        {
+            while(nums[i] > 0 && nums[i] <= nums.Count() - 1 && nums[i] != nums[nums[i] - 1])
+            {
+                var temp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[temp - 1] = temp;
+            }
+                
+        }
+        for(int i = 0 ; i < nums.Count(); i++)
+        {
+            if(nums[i] != i + 1)
+                return i + 1;
+        }
+        return nums.Count() + 1;
+    }
+}
+
+
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        if(nums.Count() == 0)
+            return 1;
 
         int max = -1;
         for(int i = 0 ; i < nums.Count() ; i++)
