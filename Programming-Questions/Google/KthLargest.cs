ffe854c02502f83f93cedc222ac5ef48ca928747
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-09 10:34:14 -0500

    Update KthLargest.cs

--- Programming-Questions/Google/KthLargest.cs
+++ Programming-Questions/Google/KthLargest.cs
@@ -5,3 +5,55 @@ public class Solution {
         return list[list.Count() - k];
     }
 }
+
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        int low = 0;
+        int high = nums.Count() - 1;
+        k = nums.Count() - k;
+        while(low < high)
+        {
+            var j = Partition(nums, low, high);
+            if(j < k)
+            {
+                low = j + 1;
+            }
+            else if(j > k)
+            {
+                high = j - 1;
+            }
+            else{
+                break;
+            }
+        }
+        return nums[k];
+    }
+    
+    public int Partition(int[] nums, int low , int high)
+    {
+        int i = low;
+        int j = high + 1;
+        while(true)
+        {
+            while(i < high && Less(nums[++i], nums[low]));
+            while(j > low && Less(nums[low], nums[--j]));
+            if(i >= j)
+                break;
+            Swap(nums,i,j);
+        }
+        Swap(nums,low,j);
+        return j;
+    }
+    
+    public bool Less(int i, int j)
+    {
+        return i < j;
+    }
+    
+    public void Swap(int[] nums, int i , int j)
+    {
+        var temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
