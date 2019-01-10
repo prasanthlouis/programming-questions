32717be7bb82bd4bb7cd8384785fcf0433c58c49
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-10 16:00:53 -0500

    Update Longest Substring Without Repeating Characters.cs

--- Programming-Questions/Google/Longest Substring Without Repeating Characters.cs
+++ Programming-Questions/Google/Longest Substring Without Repeating Characters.cs
@@ -4,6 +4,33 @@ public class Solution {
             return 0;
         if(string.IsNullOrWhiteSpace(s))
            return 1;
+        int j = 0;
+        int max = -1;
+        var dictionary = new HashSet<char>();
+        for(int z = 0 ; z < s.Length && j < s.Length ;)
+        {
+            if(!dictionary.Contains(s[j]))
+            {
+                dictionary.Add(s[j++]);
+                var length = j - z;
+                if(length > max)
+                    max = length;
+            }
+            else{
+                dictionary.Remove(s[z++]);
+            }
+        }
+        return max;
+        
+    }
+}
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if(string.IsNullOrWhiteSpace(s) && s == s.Trim())
+            return 0;
+        if(string.IsNullOrWhiteSpace(s))
+           return 1;
         int j = 1;
         int max = -1;
         var dictionary = new Dictionary<char,int>();
