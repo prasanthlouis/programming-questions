01067966a455ab2b0f83a5740df4019ac4df9daf
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-10 14:50:04 -0500

    Update Next Greater Element I.cs

--- Programming-Questions/Google/Next Greater Element I.cs
+++ Programming-Questions/Google/Next Greater Element I.cs
@@ -2,6 +2,30 @@ public class Solution {
     public int[] NextGreaterElement(int[] findNums, int[] nums) {
         var answer = new int[findNums.Count()];
         var numsList = nums.ToList();
+        var stack = new Stack<int>();
+        var dictionary = new Dictionary<int,int>();
+        for(int i = 0 ; i < nums.Count(); i++)
+        {
+            while(stack.Count() > 0 && stack.Peek() < nums[i])
+                dictionary.Add(stack.Pop(), nums[i]);
+            stack.Push(nums[i]);        
+        }
+        for(int i = 0; i < findNums.Count(); i++)
+        {
+            if(dictionary.ContainsKey(findNums[i]))
+                findNums[i] = dictionary[findNums[i]];
+            else{
+                findNums[i] = -1;
+            }
+        }
+        return findNums;
+    }
+}
+
+public class Solution {
+    public int[] NextGreaterElement(int[] findNums, int[] nums) {
+        var answer = new int[findNums.Count()];
+        var numsList = nums.ToList();
         for(int i = 0 ; i < findNums.Count(); i++)
         {
             var element = findNums[i];
