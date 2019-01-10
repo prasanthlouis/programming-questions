8c9cc3c40209de09792697b02353aa551040a3bc
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-10 10:58:49 -0500

    Create Merge Intervals.cs

---
+++ Programming-Questions/Google/Merge Intervals.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     public int start;
+ *     public int end;
+ *     public Interval() { start = 0; end = 0; }
+ *     public Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+ //nlogn - > sorting  (we don't care about n merging)
+public class Solution {
+    public IList<Interval> Merge(IList<Interval> intervals) 
+    {
+        IList<Interval> mergedIntervals = new List<Interval>();
+        if (null == intervals || intervals.Count == 0) return mergedIntervals;
+        if (intervals.Count == 1) return intervals;
+        
+        intervals = intervals.OrderBy(x => x.start).ToList();
+        foreach(var interval in intervals)
+        {
+            Console.Write(interval.start);
+            Console.Write(interval.end);
+            Console.WriteLine();
+        }
+        
+        var start = intervals[0].start;
+        var end = intervals[0].end;
+        for(int i = 1 ; i < intervals.Count; i++)
+        {
+            if(end < intervals[i].start)
+            {
+                mergedIntervals.Add(new Interval(start, end));
+                start = intervals[i].start;
+                end = intervals[i].end;
+            }
+            else{
+                end = Math.Max(end, intervals[i].end);
+            }
+        }
+        mergedIntervals.Add(new Interval(start,end));
+        return mergedIntervals;
+    }
+  
+}
