6d2b54a6d5fbbd2c2995336689cd9360aadcad7b
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-20 00:54:19 -0500

    Update Decode Ways.cs

--- Programming-Questions/Google/Decode Ways.cs
+++ Programming-Questions/Google/Decode Ways.cs
@@ -1,6 +1,33 @@
 public class Solution {
     public int NumDecodings(string s) {
         var dp = new int[s.Length + 1];
+        if(s == null || s.Length == 0) {
+            return 0;
+        }
+        dp[0] = 1;
+        dp[1] = s[0] == '0' ? 0 : 1;
+        for(int i = 2 ; i <= s.Length ; i++)
+        {
+            var element =  int.Parse(s.Substring(i-1,1));
+            var secondElement = int.Parse(s.Substring(i-2,2));
+
+            if(element > 0)
+            {
+                dp[i] += dp[i-1];           
+            }
+            if(secondElement >= 10 && secondElement <= 26){
+                dp[i] += dp[i-2];
+            }
+            
+        }
+
+        return dp[s.Length];
+    }
+}
+
+public class Solution {
+    public int NumDecodings(string s) {
+        var dp = new int[s.Length + 1];
 
         dp[s.Length] = 1;
         dp[s.Length - 1] = s[s.Length -1] == '0' ? 0 : 1;
