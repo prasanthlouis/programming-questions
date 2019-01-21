d5104e73ea5e60468bb012173b11138d9826740a
Prasanth Louis <prasanthlouis21@gmail.com>
2019-01-21 06:30:20 -0500

    Update FruitIntoBaskets.cs

--- Programming-Questions/Google/FruitIntoBaskets.cs
+++ Programming-Questions/Google/FruitIntoBaskets.cs
@@ -1,5 +1,54 @@
 public class Solution {
     public int TotalFruit(int[] tree) {
+        var start = 0;
+        var end = 0;
+        var counter = 0;
+        var dict = new Dictionary<int,int>();
+        var length = 0;
+        var head = 0;
+        var max = 0;
+        while(end < tree.Count())
+        {
+            if(!dict.ContainsKey(tree[end]))
+            {
+                dict.Add(tree[end], 1);
+                counter++;
+            }
+            else{
+                dict[tree[end]]++;
+            }
+            
+            length++;
+            
+            
+            if(counter <= 2 && length > max)
+            {
+                max = length;
+            }
+            
+            while(counter > 2)
+            {
+
+                         dict[tree[start]]--;
+                        
+                         if(dict[tree[start]] == 0)
+                         {
+                            dict.Remove(tree[start]);
+                            counter--;
+                         }
+                        start++;
+                        length--;
+
+            }
+            end++;
+
+        }
+        return max;
+    }
+}
+
+public class Solution {
+    public int TotalFruit(int[] tree) {
         var max = 0;
         var fastPointer = 0;
         var slowPointer = 0;
